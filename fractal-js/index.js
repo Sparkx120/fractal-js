1b050233c232c6181dfc29eadd0792fc7cbfcf37
James Wake <sparkx.os.120@gmail.com>
2017-12-13 00:43:19 -0500

    Refactor to Fractal.js and preliminary work on multi fractal type selection

--- fractal-js/index.js
+++ fractal-js/index.js
@@ -1,15 +1,32 @@
 import Canvas2D from "canvas-2d-framework";
-import Mandelbrot from "./Mandelbrot.js";
+import Fractal from "./Fractal.js";
 
 export default function init(canvasID){
 	//Canvas
 	const canvas2D = new Canvas2D();
 
+	
+
 	window.onload = ()=>{
-		let mandelbrot = new Mandelbrot(canvas2D);
-		mandelbrot.render();
+		const mandlBtn = document.getElementById("Mandelbrot");
+		const juliaBtn = document.getElementById("Julia");
+		let fractal = new Fractal(canvas2D);
+		fractal.render();
 		let last = 0;
-		let cb = ()=>{if(new Date().getTime()-last > 1000){last=new Date().getTime(); mandelbrot.render();}};
+		let cb = ()=>{if(new Date().getTime()-last > 1000){last=new Date().getTime(); fractal.render();}};
 		canvas2D.resizeCB = cb;
+		//TODO Work on allowing deconstruction
+		// if(mandlBtn)
+		// 	mandlBtn.onclick = ()=>{
+		// 		fractal = new Fractal(canvas2D);
+		// 		fractal.type = fractal.types.mandelbrot;
+		// 		fractal.render();
+		// 	}
+		// if(juliaBtn)
+		// 	juliaBtn.onclick = ()=>{
+		// 		fractal = new Fractal(canvas2D);
+		// 		fractal.type = fractal.types.julia;
+		// 		fractal.render();
+		// 	}
 	}
 };
